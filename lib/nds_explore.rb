f022ee7cf0d0f864ac654631f4e56219d98b60ca
Elizabeth Karen <elizabeth.michael.karen@gmail.com>
2020-02-24 23:18:09 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -1,11 +1,24 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
-require 'directors_database'
+require 'directors_database.rb'
 # Call the method directors_database to retrieve the NDS
 
 def pretty_print_nds(nds)
   # Change the code below to pretty print the nds with pp
+  pp directors_database
   nil
 end
 
 def print_first_directors_movie_titles
+    title = [ ]
+    string = "Stephen Spielberg"
+    movies.each_value do |movie|
+      if name.[:movies] == string
+        movie.movies.each_value do | v|
+    title.push(v)
+  end
+    return title
 end
+end
+end
+
+print_first_directors_movie_titles(directors_database)
